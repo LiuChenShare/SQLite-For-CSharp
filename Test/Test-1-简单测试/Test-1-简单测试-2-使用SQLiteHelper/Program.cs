1837ae2f18113ec5d66fe7060721669f7f587874
村长 <757891685@qq.com>
2018-08-22 11:04:30 +0800

    添加对SQLiteHelper的测试应用

---
+++ Test/Test-1-简单测试/Test-1-简单测试-2-使用SQLiteHelper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Test_1_简单测试_2_使用SQLiteHelper
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Chy.SQLite.SQLiteHelper.CreateDBFile("CCC");
+        }
+    }
+}
