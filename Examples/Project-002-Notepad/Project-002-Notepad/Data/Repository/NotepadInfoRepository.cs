57369a65d5ca1c376ee253087c8a1813930f1d34
村长 <757891685@qq.com>
2019-10-30 20:19:03 +0800

    基本完成记事本功能

--- Examples/Project-002-Notepad/Project-002-Notepad/Data/Repository/NotepadInfoRepository.cs
+++ Examples/Project-002-Notepad/Project-002-Notepad/Data/Repository/NotepadInfoRepository.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using Project_002_Notepad.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Project_002_Notepad.Data.Repository
 {
-    class NotepadInfoRepository
+    /// <summary>
+    /// 笔记本仓储服务
+    /// </summary>
+    public class NotepadInfoRepository
     {
+        private NotepadContext notepadContext = ContextManager.Instance.GetNotepadContext();
+
+        public List<NotepadInfo> GetInfos()
+        {
+            return notepadContext.NotepadInfo.OrderByDescending(x =>x.CreateTime).ToList();
+        }
+
+        public bool SetInfo(string txt)
+        {
+            notepadContext.NotepadInfo.Add(new NotepadInfo
+            {
+                Id = Guid.NewGuid().ToString(),
+                NotepadContent = txt,
+                CreateTime = DateTime.Now
+            });
+            notepadContext.SaveChanges();
+            return true;
+        }
     }
 }
--- Examples/Project-002-Notepad/Project-002-Notepad/Form1.cs
+++ Examples/Project-002-Notepad/Project-002-Notepad/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Project_002_Notepad.Data;
+using Project_002_Notepad.Data.Repository;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,7 +20,27 @@ namespace Project_002_Notepad
 
         private void button1_Click(object sender, EventArgs e)
         {
-            NotepadContext notepadContext = ContextManager.Instance.GetNotepadContext();
+            NotepadInfoRepository notepadInfoRepository = new NotepadInfoRepository();
+            var infos = notepadInfoRepository.GetInfos();
+            listBox1.Items.Clear();
+            foreach (var item in infos)
+            {
+                listBox1.Items.Add($"【{item.CreateTime.ToString("yyyy-MM-dd HH:mm:ss")}】{item.NotepadContent}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text)) {
+                NotepadInfoRepository notepadInfoRepository = new NotepadInfoRepository();
+                notepadInfoRepository.SetInfo(textBox1.Text);
+                button1_Click(null, null);
+            }
+            else
+            {
+                MessageBox.Show("大兄dei，你得写字啊", "系统提示");
+            }
+            textBox1.Clear();
         }
     }
 }
